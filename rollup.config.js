9eb58a55efcb6f0bf88758011d9e25dbf6c43cde
zhaoxueyu <312364705@qq.com>
2022-01-13 22:41:47 +0800

    no message

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,27 @@ import path from 'path'
 
 const { TARGET, FORMATS, SOURCEMAP } = process.env
 
-const formats = FORMATS && FORMATS.split(',')
-const sourcemap = SOURCEMAP
-const target = TARGET
-
-console.log('formats', formats)
+// const sourcemap = SOURCEMAP
 
 const path_packages = path.resolve(__dirname, 'packages')
-const path_target = path.resolve(path_packages, target)
+const path_target = path.resolve(path_packages, TARGET)
+const resolve = p => path.resolve(path_target, p)
 
+const pkg = require(resolve('package.json'))
 
+const output_config = {
+  'esm-bundler': {
+    file: resolve(`dist/${TARGET}.esm-bundler.js`),
+    format: 'es'
+  },
+  'cjs': {
+    file: resolve(`dist/${TARGET}.cjs.js`),
+    format: 'cjs'
+  },
+  'global': {
+    file: resolve(`dist/${TARGET}.global.js`),
+    format: 'global'
+  },
+}
+
+const formats = (FORMATS && FORMATS.split(',')) || pkg.buildOptions.formats
